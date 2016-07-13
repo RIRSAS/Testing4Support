0e0ce261a45c38e4bbd2b12f10f02fb65048cb26
RIRSAS <soratosyo9@gmail.com>
2016-07-13 22:15:50 +0900

    First Commit

---
+++ app/src/main/java/com/rirsas/user/testing4support/RecyclerAdapter.java
@@ -0,0 +1,25 @@
+package com.rirsas.user.testing4support;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by USER on 2016/07/13.
+ */
+public class RecyclerAdapter extends android.support.v7.widget.RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
